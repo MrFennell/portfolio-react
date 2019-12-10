3b6936a21d1df33e00927e5e20f1d939ac037ed2
Ryan <rfennell001@gmail.com>
2019-12-10 16:23:33 -0500

    Sass and shortcuts

--- src/components/header.js
+++ src/components/header.js
@@ -16,6 +16,7 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
+    <button type="button" class="btn btn-primary">sadsadsa</button>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -15,6 +16,8 @@ const IndexPage = () => (
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
+
+    
   </Layout>
 )
 
